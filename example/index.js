68a91578c69173a735cfd5015baa88c84d4bb7db
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-04-23 15:01:44 +0530

    chore: remove example app

--- example/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const phase = require('../dist/index');
-const testData = 'hello world';
-phase.encrypt(testData, 'test').then((encryptedData) => {
-    console.log(`encrypted ${testData}`);
-    console.log(encryptedData);
-});
